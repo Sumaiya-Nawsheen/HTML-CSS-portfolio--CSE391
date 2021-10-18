93456e91d7aaf8ceb3f17374f3bcc3ea2f8f6324
Sumaiya-Nawsheen <sumaiya.naw252@gmail.com>
2021-10-18 22:16:22 +0600

    portfolio

---
+++ portfolio.js
@@ -0,0 +1,30 @@
+
+
+function navFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  
+  // last modification date
+    document.getElementById("modify").innerHTML = document.lastModified;;
+ 
+// page's location
+    document.getElementById("location").innerHTML = window.location.pathname;
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
